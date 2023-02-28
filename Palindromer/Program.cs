ff8d572322b67868924acf75fe35b2cede3509be
Tommerxxx1 <tommer1986gmail.com>
2023-02-28 20:48:25 +0100

    Add project files.

---
+++ Palindromer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Palindromer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast en tekst streng for at tjekke om der er tale, om et palindrom:");
+
+            string orginal = Convert.ToString(Console.ReadLine());
+            string svar = orginal;
+            svar.ToLower();
+
+            string omvendt = new string(svar.Reverse().ToArray());
+            bool tjek = svar == omvendt;
+            if (tjek) {
+                Console.WriteLine($"{orginal} er et palindrom"); 
+                       }
+            else 
+            { 
+                Console.WriteLine($"{orginal} er IKKE et palindrom"); 
+            }
+
+        }
+    }
+}
